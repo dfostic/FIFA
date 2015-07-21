37c2a40b2ccc538842586e22b221597d622ea6ce
DFostic1 <DFostic1@WL101801.ad.ypg.com>
2015-07-20 20:17:10 -0400

    Adding Team and Trainer classes

---
+++ src/main/java/com/dfostic/beans/Team.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.dfostic.beans;
+
+/**
+ *
+ * @author DFostic1
+ */
+public class Team {
+    
+    private String name;
+    private int foundationYear;
+    
+}
---
+++ src/main/java/com/dfostic/beans/Trainer.java
@@ -0,0 +1,14 @@
+package com.dfostic.beans;
+
+/**
+ *
+ * @author DFostic1
+ */
+public class Trainer {
+    
+    private String firstName;
+    private String lastName;
+    private int age;
+    
+    
+}
